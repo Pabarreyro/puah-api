ca8ef41c1bc15117e1e7cc7efdfa5a5e5671ef55
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-11 15:55:48 -0700

    Register basic CRUD methods

---
+++ src/main/java/dao/RegionDao.java
@@ -0,0 +1,25 @@
+package dao;
+
+import models.Organization;
+import models.Region;
+
+import java.util.List;
+
+public interface RegionDao {
+
+    // CREATE
+    void add(Region region);
+//    void addRegionToOrganization(Region region, Organization organization);
+
+    // READ
+    List<Region> getAll();
+    Region findById(int id);
+//    List<Region> getAllByOrganizationId(int organizationId);
+
+    // UPDATE
+    void update(String name);
+
+    // DELETE
+    void deleteById(int id);
+    void clearAll();
+}
