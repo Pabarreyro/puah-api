99ed0cbf8294e3afd77bd0b4849d945cf3ff014b
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-11 15:00:11 -0700

    Add tests for constructors and setters

--- src/test/java/models/OrganizationTest.java
+++ src/test/java/models/OrganizationTest.java
@@ -1,17 +1,72 @@
 package models;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.*;
 
 import static org.junit.Assert.*;
 
 public class OrganizationTest {
-
-    @Before
-    public void setUp() throws Exception {
+    @Test
+    public void newOrganization_instantiatesCorrectly() {
+        Organization testOrganization = newOrganizationFull();
+        assertEquals("www.ccc.org", testOrganization.getWebsite());
+        assertEquals("info@ccc.org", testOrganization.getEmail());
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void newOrganization_instantiatesCorrectlyWithoutEmailOrWebsite() {
+        Organization testOrganization = newOrganizationPartial();
+        assertEquals("website unavailable", testOrganization.getWebsite());
+        assertEquals("email unavailable", testOrganization.getEmail());
+    }
+
+    @Test
+    public void setName_setsNameCorrectly() {
+        Organization testOrganization = newOrganizationFull();
+        testOrganization.setName("Coalition of Communities of Color");
+        assertEquals("Coalition of Communities of Color", testOrganization.getName());
+    }
+
+    @Test
+    public void setAddress_setsAddressCorrectly() {
+        Organization testOrganization = newOrganizationFull();
+        testOrganization.setAddress("211 Nw 3rd Ave");
+        assertEquals("211 Nw 3rd Ave", testOrganization.getAddress());
+    }
+
+    @Test
+    public void setZip_setsZipCorrectly() {
+        Organization testOrganization = newOrganizationFull();
+        testOrganization.setZip("97202");
+        assertEquals("97202", testOrganization.getZip());
+    }
+
+    @Test
+    public void setPhone_setsPhoneCorrectly() {
+        Organization testOrganization = newOrganizationFull();
+        testOrganization.setPhone("505-223-5840");
+        assertEquals("505-223-5840", testOrganization.getPhone());
+    }
+
+    @Test
+    public void setWebsite_setsWebsiteCorrectly() {
+        Organization testOrganization = newOrganizationFull();
+        testOrganization.setWebsite("ccc.com");
+        assertEquals("ccc.com", testOrganization.getWebsite());
+    }
+
+    @Test
+    public void setEmail_setsEmailCorrectly() {
+        Organization testOrganization = newOrganizationFull();
+        testOrganization.setEmail("contact@ccc.org");
+        assertEquals("contact@ccc.org", testOrganization.getEmail());
+    }
+
+    // Helpers
+    public Organization newOrganizationFull() {
+        return new Organization("CCC", "201 NW 2nd Ave", "97201", "503-260-5690", "www.ccc.org", "info@ccc.org");
+    }
+
+    public Organization newOrganizationPartial() {
+        return new Organization("CCC", "201 NW 2nd Ave", "97201", "503-260-5690");
     }
 }
