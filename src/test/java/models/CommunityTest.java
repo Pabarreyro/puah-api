9904f91143de703ef437f4f06ec640c44c2b5ab0
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-11 14:47:27 -0700

    Add tests for constructors and setters

--- src/test/java/models/CommunityTest.java
+++ src/test/java/models/CommunityTest.java
@@ -2,16 +2,42 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CommunityTest {
 
-    @Before
-    public void setUp() throws Exception {
+    @Test
+    public void newCommunity_instantiatesCorrectly() {
+        Community testCommunity = newCommunity();
+        assertEquals("non-binary", testCommunity.getName());
+        assertEquals("gender", testCommunity.getType());
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void setName_setsNameCorrectly() {
+        Community testCommunity = newCommunity();
+        testCommunity.setName("cis-gendered female");
+        assertEquals("cis-gendered female", testCommunity.getName());
+    }
+
+    @Test
+    public void setType_setsNameCorrectly() {
+        Community testCommunity = newCommunity();
+        testCommunity.setType("gender identity");
+        assertEquals("gender identity", testCommunity.getType());
+    }
+
+    @Test
+    public void setId_setsNameCorrectly() {
+        Community testCommunity = newCommunity();
+        testCommunity.setId(1);
+        assertEquals(1, testCommunity.getId());
+    }
+
+    // Helpers
+    public Community newCommunity() {
+        return new Community("non-binary", "gender");
     }
 }
