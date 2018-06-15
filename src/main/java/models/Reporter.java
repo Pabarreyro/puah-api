88d473634b8a31ec0eaa9abefbeae325515f28bd
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-15 15:40:52 -0700

    Remove contact information fields

--- src/main/java/models/Reporter.java
+++ src/main/java/models/Reporter.java
@@ -1,10 +1,11 @@
 package models;
 
 public class Reporter {
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phone;
     private String location;
     private int age;
+
+    public Reporter(String location, int age) {
+        this.location = location;
+        this.age = age;
+    }
 }
