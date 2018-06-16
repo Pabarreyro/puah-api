ec9bba985c7c281d69e5f2ccbad3fef59ef8fa21
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-16 13:45:04 -0700

    Add constructor

--- src/main/java/dao/Sql2oReportDao.java
+++ src/main/java/dao/Sql2oReportDao.java
@@ -1,7 +1,10 @@
 package dao;
 
 import models.Report;
+import org.sql2o.Sql2o;
 
 public class Sql2oReportDao implements ReportDao{
+    private final Sql2o sql2o;
 
+    public Sql2oReportDao(Sql2o sql2o) { this.sql2o = sql2o; }
 }
