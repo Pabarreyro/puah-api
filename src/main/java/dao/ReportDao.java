ccfa27ffbd662d14e831b61308734c377256ec58
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-16 12:29:51 -0700

    Register DAO methods

--- src/main/java/dao/ReportDao.java
+++ src/main/java/dao/ReportDao.java
@@ -1,4 +1,25 @@
 package dao;
 
+import models.Report;
+
+import java.util.List;
+
 public interface ReportDao {
+
+    // CREATE
+    public void add(Report report);
+
+    // READ
+    public List<Report> getAll();
+    public Report findById(int reportId);
+    public Report findByConfirmationNumber (String confirmationNumber);
+
+    // UPDATE
+    public void update(Report report);
+
+    // DELETE
+    public void deleteById(int reportId);
+    public void deleteByConfirmationNumber(String confirmationNumber);
+    public void clearAll();
+
 }
