584581fe832543ca9415dea25d576a3d470cf293
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-17 09:47:29 -0700

    Refactored from abstract to complete class

--- src/main/java/models/Report.java
+++ src/main/java/models/Report.java
@@ -1,25 +1,287 @@
 package models;
 
-public abstract class Report {
-    public String type;
-    public String reporterRole;
-    public int reporterAge;
-    public String reporterLocation;
-    public String incidentDate;
-    public String incidentTime;
-    public String incidentCrossStreets;
-    public String incidentSetting;
-    public String incidentType;
-    public String incidentTypeNotes;
-    public String incidentMotivation;
-    public String incidentMotivationNotes;
-    public boolean injuryOccurred;
-    public String injuryNotes;
-    public boolean damagesOccurred;
-    public String damagesNotes;
-    public boolean officiallyReported;
-    public String officialReportNotes;
-    public String additionalNotes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Report {
+    private String type;
+    private String reporterRole;
+    private int reporterAge;
+    private String reporterLocation;
+    private String incidentDate;
+    private String incidentTime;
+    private String incidentCrossStreets;
+    private String incidentSetting;
+    private String incidentType;
+    private String incidentTypeNotes;
+    private String incidentMotivation;
+    private String incidentMotivationNotes;
+    private boolean injuryOccurred;
+    private String injuryNotes;
+    private boolean damagesOccurred;
+    private String damagesNotes;
+    private boolean officiallyReported;
+    private String officialReportNotes;
+    private String additionalNotes;
+    private long dateTimeFiled;
+    private String formattedDateTime;
+    private String confirmationCode;
+
+    private int id;
+    private List<Community> identifies;
+    private List<Organization> organizations;
+    private List<Contact> contacts;
+
+
+    public Report(String type, String reporterRole, int reporterAge, String reporterLocation, String incidentDate, String incidentTime, String incidentCrossStreets, String incidentSetting, String incidentType, String incidentTypeNotes, String incidentMotivation, String incidentMotivationNotes, boolean injuryOccurred, String injuryNotes, boolean damagesOccurred, String damagesNotes, boolean officiallyReported, String officialReportNotes, String additionalNotes) {
+        this.type = type;
+        this.reporterRole = reporterRole;
+        this.reporterAge = reporterAge;
+        this.reporterLocation = reporterLocation;
+        this.incidentDate = incidentDate;
+        this.incidentTime = incidentTime;
+        this.incidentCrossStreets = incidentCrossStreets;
+        this.incidentSetting = incidentSetting;
+        this.incidentType = incidentType;
+        this.incidentTypeNotes = incidentTypeNotes;
+        this.incidentMotivation = incidentMotivation;
+        this.incidentMotivationNotes = incidentMotivationNotes;
+        this.injuryOccurred = injuryOccurred;
+        this.injuryNotes = injuryNotes;
+        this.damagesOccurred = damagesOccurred;
+        this.damagesNotes = damagesNotes;
+        this.officiallyReported = officiallyReported;
+        this.officialReportNotes = officialReportNotes;
+        this.additionalNotes = additionalNotes;
+
+        this.dateTimeFiled = System.currentTimeMillis();
+        this.confirmationCode = "";
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getReporterRole() {
+        return reporterRole;
+    }
+
+    public void setReporterRole(String reporterRole) {
+        this.reporterRole = reporterRole;
+    }
+
+    public int getReporterAge() {
+        return reporterAge;
+    }
+
+    public void setReporterAge(int reporterAge) {
+        this.reporterAge = reporterAge;
+    }
+
+    public String getReporterLocation() {
+        return reporterLocation;
+    }
+
+    public void setReporterLocation(String reporterLocation) {
+        this.reporterLocation = reporterLocation;
+    }
+
+    public String getIncidentDate() {
+        return incidentDate;
+    }
+
+    public void setIncidentDate(String incidentDate) {
+        this.incidentDate = incidentDate;
+    }
+
+    public String getIncidentTime() {
+        return incidentTime;
+    }
+
+    public void setIncidentTime(String incidentTime) {
+        this.incidentTime = incidentTime;
+    }
+
+    public String getIncidentCrossStreets() {
+        return incidentCrossStreets;
+    }
+
+    public void setIncidentCrossStreets(String incidentCrossStreets) {
+        this.incidentCrossStreets = incidentCrossStreets;
+    }
+
+    public String getIncidentSetting() {
+        return incidentSetting;
+    }
+
+    public void setIncidentSetting(String incidentSetting) {
+        this.incidentSetting = incidentSetting;
+    }
+
+    public String getIncidentType() {
+        return incidentType;
+    }
+
+    public void setIncidentType(String incidentType) {
+        this.incidentType = incidentType;
+    }
+
+    public String getIncidentTypeNotes() {
+        return incidentTypeNotes;
+    }
+
+    public void setIncidentTypeNotes(String incidentTypeNotes) {
+        this.incidentTypeNotes = incidentTypeNotes;
+    }
+
+    public String getIncidentMotivation() {
+        return incidentMotivation;
+    }
+
+    public void setIncidentMotivation(String incidentMotivation) {
+        this.incidentMotivation = incidentMotivation;
+    }
+
+    public String getIncidentMotivationNotes() {
+        return incidentMotivationNotes;
+    }
+
+    public void setIncidentMotivationNotes(String incidentMotivationNotes) {
+        this.incidentMotivationNotes = incidentMotivationNotes;
+    }
+
+    public boolean isInjuryOccurred() {
+        return injuryOccurred;
+    }
+
+    public void setInjuryOccurred(boolean injuryOccurred) {
+        this.injuryOccurred = injuryOccurred;
+    }
+
+    public String getInjuryNotes() {
+        return injuryNotes;
+    }
+
+    public void setInjuryNotes(String injuryNotes) {
+        this.injuryNotes = injuryNotes;
+    }
+
+    public boolean isDamagesOccurred() {
+        return damagesOccurred;
+    }
+
+    public void setDamagesOccurred(boolean damagesOccurred) {
+        this.damagesOccurred = damagesOccurred;
+    }
+
+    public String getDamagesNotes() {
+        return damagesNotes;
+    }
+
+    public void setDamagesNotes(String damagesNotes) {
+        this.damagesNotes = damagesNotes;
+    }
+
+    public boolean isOfficiallyReported() {
+        return officiallyReported;
+    }
+
+    public void setOfficiallyReported(boolean officiallyReported) {
+        this.officiallyReported = officiallyReported;
+    }
+
+    public String getOfficialReportNotes() {
+        return officialReportNotes;
+    }
+
+    public void setOfficialReportNotes(String officialReportNotes) {
+        this.officialReportNotes = officialReportNotes;
+    }
+
+    public String getAdditionalNotes() {
+        return additionalNotes;
+    }
+
+    public void setAdditionalNotes(String additionalNotes) {
+        this.additionalNotes = additionalNotes;
+    }
+
+    public long getDateTimeFiled() {
+        return dateTimeFiled;
+    }
+
+    public void setDateTimeFiled(long dateTimeFiled) {
+        this.dateTimeFiled = dateTimeFiled;
+    }
+
+    public String getFormattedDateTime() {
+        return formattedDateTime;
+    }
+
+    public void setFormattedDateTime(String formattedDateTime) {
+        this.formattedDateTime = formattedDateTime;
+    }
+
+    public String getConfirmationCode() {
+        return confirmationCode;
+    }
+
+    public void setConfirmationCode(String confirmationCode) {
+        this.confirmationCode = confirmationCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Community> getIdentifies() {
+        return identifies;
+    }
+
+    public void setIdentifies(List<Community> identifies) {
+        this.identifies = identifies;
+    }
+
+    public List<Organization> getOrganizations() {
+        return organizations;
+    }
+
+    public void setOrganizations(List<Organization> organizations) {
+        this.organizations = organizations;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Report report = (Report) o;
+        return dateTimeFiled == report.dateTimeFiled &&
+                id == report.id &&
+                Objects.equals(type, report.type) &&
+                Objects.equals(confirmationCode, report.confirmationCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, dateTimeFiled, confirmationCode, id);
+    }
 }
 
 
