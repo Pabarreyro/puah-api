575f10ad24914b336c6b4ee033bd9ca37711d980
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-17 14:03:15 -0700

    Refactor constructor; add getters, setters, equals(), hashCode()

--- src/main/java/models/Contact.java
+++ src/main/java/models/Contact.java
@@ -1,16 +1,75 @@
 package models;
 
-public class Contact extends Reporter{
+import java.util.Objects;
+
+public class Contact {
     private String firstName;
     private String lastName;
     private String email;
     private String phone;
+    private int id;
 
-    public Contact(String firstName, String lastName, String email, String phone, String location, int age) {
-        super(location, age);
+    public Contact(String firstName, String lastName, String email, String phone) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return id == contact.id &&
+                Objects.equals(firstName, contact.firstName) &&
+                Objects.equals(lastName, contact.lastName) &&
+                Objects.equals(email, contact.email) &&
+                Objects.equals(phone, contact.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, email, phone, id);
     }
 }
