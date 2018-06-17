d37b444caf022d68efa680e775864161bc28dac1
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-17 14:21:32 -0700

    Register basic CRUD methods

--- src/main/java/dao/ContactDao.java
+++ src/main/java/dao/ContactDao.java
@@ -1,5 +1,23 @@
 package dao;
 
+import models.Contact;
+
+import java.util.List;
+
 public interface ContactDao {
+
+    // CREATE
+    void add(Contact contact);
+
+    // READ
+    List<Contact> getAll();
+    Contact findById(int id);
+
+    // UPDATE
+    void update(Contact contact);
+
+    // DELETE
+    void deleteById(int contactId);
+    void clearAll();
 
 }
